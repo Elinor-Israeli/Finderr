c501827ae2aa57f1d7ed1d439ab275392c81a9b9
Elinor <israeli.elinor@gmail.com>
2024-11-12 13:52:21 +0200

    Fixing style

--- frontend-react/src/cmps/gig/GigPreview.jsx
+++ frontend-react/src/cmps/gig/GigPreview.jsx
@@ -43,7 +43,7 @@ export function GigPreview({ gig }) {
         }
     }
 
-    
+
     // const levelNumber = parseInt(gig.owner.level.replace(/\D/g, ''), 10)
     const levelNumber = 2
     // console.log(gig)
@@ -62,10 +62,12 @@ export function GigPreview({ gig }) {
                         }}
                         title="save to my list"
                         style={{
-                        color: heart ? "#ffffff" : "#ffffff",
-                       
+                            color: heart ? "#ffffff" : "#ffffff",
+
                             // transition: "color .6s ease",
-                            fontSize: '25px'
+                            fontSize: '25px',
+                            margin: '10px',
+                            padding: '10px'
                         }}
                     >
                         <FaRegHeart />
@@ -96,7 +98,7 @@ export function GigPreview({ gig }) {
                             </span>
                         </div>
                     </div>
-                    
+
                 </div>
                 <Link className="title" to={`/gig/${gig._id}`}>
                     <div className="long-txt">
@@ -105,8 +107,8 @@ export function GigPreview({ gig }) {
                 </Link>
                 <div className="rate">
                     {/* <Box sx={{ '& > legend': { mt: '5px', marginTop: '5px', color: 'black' }, '& .MuiRating-icon': { color: 'red',marginRight: '4px' } }}> */}
-                    <svg width="16" height="15" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"/></svg>
-                        {/* <Rating
+                    <svg width="16" height="15" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z" /></svg>
+                    {/* <Rating
                             value={gig.owner.rate}
                             name="half-rating-read"
                             size="small"

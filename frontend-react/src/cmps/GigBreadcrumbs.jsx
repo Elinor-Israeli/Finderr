406fbdd453cbe7bc34cc9473cf826caa4d2d6a57
Elinor <israeli.elinor@gmail.com>
2024-11-17 22:10:07 +0200

    Style the explore page

--- frontend-react/src/cmps/GigBreadcrumbs.jsx
+++ frontend-react/src/cmps/GigBreadcrumbs.jsx
@@ -54,9 +54,9 @@ export function GigBreadcrumbs({ gigs }) {
     return (
         
          <nav className='nav-links-breadcrumbs'>
-                <Link to="/">< svg width="13" height="13" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.666 13.548H3.333a.667.667 0 01-.667-.666v-6h-2L7.552.621a.667.667 0 01.897 0l6.885 6.26h-2v6a.667.667 0 01-.667.666zm-4-1.333H12V5.653L8 2.017 4 5.653v6.562h3.333v-4h1.333v4z" fill="#404145"/></svg></Link>
+                <Link to="/" className='home-breadcrumbs'>< svg width="13" height="13" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.666 13.548H3.333a.667.667 0 01-.667-.666v-6h-2L7.552.621a.667.667 0 01.897 0l6.885 6.26h-2v6a.667.667 0 01-.667.666zm-4-1.333H12V5.653L8 2.017 4 5.653v6.562h3.333v-4h1.333v4z" fill="#404145"/></svg></Link>
                 {' / '}
-                <Link to="/graphics-design">Graphics & design</Link>
+                <Link to="/graphics-design"> Graphics & design</Link>
                 {' / '}
                 <Link to="/gig">Logo Design</Link>
             </nav>

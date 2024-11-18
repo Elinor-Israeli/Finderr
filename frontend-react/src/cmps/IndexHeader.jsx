ee5e22494df3805ab0cacf61c7c143ba7e47b8bc
Refael Levi <refael14levi@gmail.com>
2024-11-18 11:13:37 +0200

    fix bug

--- frontend-react/src/cmps/IndexHeader.jsx
+++ frontend-react/src/cmps/IndexHeader.jsx
@@ -169,15 +169,12 @@ export function IndexHeader({ onSetFilter, isSticky }) {
     function handleOrder() {
         setIsOrder(prev => !prev)
     }
-    return ( 
-        
-            <nav className="app-header-nav" ref={headerRef}>
-            <button className=" menu-toggle-btn"
-                onClick={() => onToggleMenu()}></button>
-            {(windowSize < 900) && isOpenMenu &&
-                <AppHeaderMobile onToggleMenu={onToggleMenu} user={user} onLogout={onLogout} onOpenModal={onOpenModal} setIsSignup={setIsSignup} />}
-
-
+    return (
+        //     <nav className="app-header-nav" ref={headerRef}>
+        //     <button className=" menu-toggle-btn"
+        //         onClick={() => onToggleMenu()}></button>
+        //     {/* {(windowSize < 900) && isOpenMenu &&
+        //         <AppHeaderMobile onToggleMenu={onToggleMenu} user={user} onLogout={onLogout} onOpenModal={onOpenModal} setIsSignup={setIsSignup} />} */}
         //     <div className="app-header-main">
         //         {(windowSize > 900) && <Link to="/gig"
         //             onClick={() => {
@@ -194,7 +191,6 @@ export function IndexHeader({ onSetFilter, isSticky }) {
         //                         src={heart ? "./img/red_heart.png" : "./img/gray_heart.png"}
         //                         alt="Heart"
         //                         className="heart-img"
-
         //                     /> */}
         //                     </Link>
         //                 {(windowSize > 900) && <div className="user-header-img">
@@ -227,7 +223,6 @@ export function IndexHeader({ onSetFilter, isSticky }) {
                             <span className='logo-dot'>.</span>
                         </div>
                     </Link>
-
                     {/* {pathname === '/' && showSearch && ( */}
                     <form className="index-search" onSubmit={onSubmitFilter}>
                         <div className="search-index-input">
@@ -270,77 +265,20 @@ export function IndexHeader({ onSetFilter, isSticky }) {
                     </div>
                 </div>
             </div>
-        </nav>
-        // <section className={`my-header main-layout `}>
-        //     <div className="index-header main layout ">
-        //         <div className={`index-header-container main-layout ${isSticky ? 'sticky' : ''}`}>
-
-        //             <Link to="/">
-        //                 <div className="logo">
-        //                     <span className='logo-text'>finderr</span>
-        //                     <span className='logo-dot'>.</span>
-        //                 </div>
-        //             </Link>
-
-        //             {/* {pathname === '/' && showSearch && ( */}
-        //             <form className="index-search" onSubmit={onSubmitFilter}>
-        //                 <div className="search-index-input">
-        //                     <input
-        //                         type="text"
-        //                         className={`gig-search ${pathname !== '/' ? 'long-placeholder' : ''}`}
-        //                         id="title"
-        //                         name="title"
-        //                         placeholder={onPlaceholder()}
-        //                         value={filterByToEdit.title}
-        //                         onChange={handleChange}
-        //                         ref={elInputRef}
-        //                     />
-        //                     <button className='btn-index-search'>
-        //                         <span>
-        //                             <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="white">
-        //                                 <path d="m15.89 14.653-3.793-3.794a.37.37 0 0 0-.266-.109h-.412A6.499 6.499 0 0 0 6.5 0C2.91 0 0 2.91 0 6.5a6.499 6.499 0 0 0 10.75 4.919v.412c0 .1.04.194.11.266l3.793 3.794a.375.375 0 0 0 .531 0l.707-.707a.375.375 0 0 0 0-.53ZM6.5 11.5c-2.763 0-5-2.238-5-5 0-2.763 2.237-5 5-5 2.762 0 5 2.237 5 5 0 2.762-2.238 5-5 5Z"></path>
-        //                             </svg>
-        //                         </span>
-        //                     </button>
-        //                 </div>
-        //             </form>
-        //             {/* )} */}
-        //             <div className="links">
-        //                 <span>Fiver pro</span>
-        //                 <Link to="gig"><span>Explore</span></Link>
-        //                 {user &&
-        //                     <>
-        //                         <div className="user-orders">
-        //                             <Link onClick={handleOrder}>Orders</Link>
-        //                             {isOrder && <UserBuyGig />}
-        //                         </div>
-        //                         <Link to="/wishlist" className="fa-regular heart" title="save to list"></Link>
-        //                     </>
-        //                 }
-        //                 <span>English</span>
-        //                 <Link to="gig"> <span>Become a Seller</span></Link>
-        //                 <span>Sign in</span>
-        //                 <button className='join-btn-index-header'>Join</button>
-        //             </div>
-        //         </div>
-        //     </div>
-
-
-        //     {/* <div className={`full`}>
-
-        //         {pathname === '/' ? (
-        //             <div style={{ position: 'fixed', top: '85px', left: '0', width: '100%', zIndex: 10000, backgroundColor: 'white', display: isCategoryMenuVisible ? 'block' : 'none' }}>
-        //                 {isCategoryMenuVisible && <GigCategoryMenu onSetFilter={onSetFilter} />}
-        //             </div>
-        //         ) : (
-        //             <div>
-        //                 <GigCategoryMenu onSetFilter={onSetFilter} />
-        //             </div>
-        //         )}
-        //     </div> */}
-        //     <div>
-        //                 <GigCategoryMenu onSetFilter={onSetFilter} />
-        //             </div>
-        // </section>
+            {/* <div className={`full`}>
+                {pathname === '/' ? (
+                    <div style={{ position: 'fixed', top: '85px', left: '0', width: '100%', zIndex: 10000, backgroundColor: 'white', display: isCategoryMenuVisible ? 'block' : 'none' }}>
+                        {isCategoryMenuVisible && <GigCategoryMenu onSetFilter={onSetFilter} />}
+                    </div>
+                ) : (
+                    <div>
+                        <GigCategoryMenu onSetFilter={onSetFilter} />
+                    </div>
+                )}
+            </div> */}
+            <div>
+                        <GigCategoryMenu onSetFilter={onSetFilter} />
+                    </div>
+        </section>
     )
 }

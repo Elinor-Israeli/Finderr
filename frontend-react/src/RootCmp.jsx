10f7892ef6859af33992309bc4cd48d9c7540da2
Elinor <israeli.elinor@gmail.com>
2024-11-19 22:51:09 +0200

    individual user and seller

--- frontend-react/src/RootCmp.jsx
+++ frontend-react/src/RootCmp.jsx
@@ -50,48 +50,7 @@ export function RootCmp() {
                 </Routes>
             </main>
             {!isPaymentPage && <AppFooter />}
-            {/* <AppFooter /> */}
         </div>
     )
 }
-
-// import React from 'react'
-// import { Routes, Route, useLocation } from 'react-router'
-
-// import { HomePage } from './pages/HomePage'
-// import { GigIndex } from './pages/gig/GigIndex'
-// import { GigDetails } from './pages/gig/GigDetails'
-// import { AppFooter } from './cmps/AppFooter'
-// import { IndexHeader } from './cmps/IndexHeader'
-// import { GigEdit } from './cmps/gig/GigEdit'
-// import { GigPayment } from './pages/gig/GigPayment'
-// import { UserIndex } from './pages/UserIndex'
-// import UserWishList from './pages/UserWishList'
-
-// export function RootCmp() {
-//     const { pathname } = useLocation();
-    
-//     const isHomePage = pathname === '/'; // Toggle for homepage, otherwise regular
-
-//     return (
-//         <div className="main-container main-layout full">
-//             {/* Conditionally apply fixed header only on homepage */}
-//             {isHomePage ? <IndexHeader fixed={true} /> : <IndexHeader fixed={false} />}
-            
-//             <main>
-//                 <Routes>
-//                     <Route path="/" element={<HomePage />} />
-//                     <Route path="/gig" element={<GigIndex />} />
-//                     <Route path="/gig/:gigId" element={<GigDetails />} />
-//                     <Route path="/gig/edit/:gigId" element={<GigEdit />} />
-//                     <Route path="/gig/edit" element={<GigEdit />} />
-//                     <Route path="/payment/:gigId" element={<GigPayment />} />
-//                     <Route path="/wishlist" element={<UserWishList />} />
-//                     <Route path="/user/:userId" element={<UserIndex />} />
-//                 </Routes>
-//             </main>
-//             <AppFooter />
-//         </div>
-//     );
-// }
 

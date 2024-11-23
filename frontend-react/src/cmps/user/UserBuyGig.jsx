163ad3671c2251f0489b59f7fec4d3097b556ed3
Refael Levi <refael14levi@gmail.com>
2024-11-24 01:25:16 +0200

    add style to orders

--- frontend-react/src/cmps/user/UserBuyGig.jsx
+++ frontend-react/src/cmps/user/UserBuyGig.jsx
@@ -22,8 +22,8 @@ export function UserBuyGig() {
         <img className="buy-order-img" src={order.gig.imgUrl[0]} />
         <div className="buy-order-info">
           <div>{order.gig.title}</div>
-          <div className="buy-order-date">
-            {moment(order.createdAt).format('MMMM Do YYYY, h:mm:ss a')}
+          <div className="buy-order-date" style={{ color: 'blue', fontStyle: 'italic' }}>
+         {moment(order.createdAt).format('MMMM Do YYYY, h:mm:ss a')}
           </div>
           <div className="buy-order-seller-status">
             <div className="buy-order-sellername">{order.seller.fullname}</div>

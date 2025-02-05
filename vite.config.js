c5896903d2a762af3d04a4000276b89f96730652
Elinor <israeli.elinor@gmail.com>
2025-02-05 17:04:50 +0200

    change path of vite build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
 	plugins: [react(), svgr()],
 	build: {
-		outDir: '../../backend-finderr/public',
+		outDir: '../Backend-Finderr/public',
 		emptyOutDir: true,
 	},
 })

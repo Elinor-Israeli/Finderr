434287e8a8e99d066a20910f060a526dc7cb855d
Elinor <israeli.elinor@gmail.com>
2025-02-05 16:26:12 +0200

    Fix msg

--- frontend-react/src/cmps/UserMsg.jsx
+++ frontend-react/src/cmps/UserMsg.jsx
@@ -1,12 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { socketService, SOCKET_EMIT_ORDER_WATCH,SOCKET_EVENT_ORDER_FROM_YOU  } from '../services/socket.service'
 import { HiMiniXMark } from "react-icons/hi2"
-import { showSuccessMsg } from '../services/event-bus.service'
+import { eventBus,  showSuccessMsg } from '../services/event-bus.service'
 
 export function UserMsg() {
   const [msg, setMsg] = useState(null)
+  const timeoutIdRef = useRef()
 
   useEffect(() => {
+    const unsubscribe = eventBus.on('show-msg', (msg) => {
+      setMsg(msg)
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+      if (timeoutIdRef.current) {
+        timeoutIdRef.current = null
+        clearTimeout(timeoutIdRef.current)
+      }
+      timeoutIdRef.current = setTimeout(closeMsg, 3000)
+    })
+
     socketService.on(SOCKET_EVENT_ORDER_FROM_YOU, (userName) => {
       showSuccessMsg(`New order from ${userName}`)
     })

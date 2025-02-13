81a424fe90f0bece09e5a6891dfa43a9cf62274e
Elinor <israeli.elinor@gmail.com>
2025-02-13 15:55:10 +0200

    Change build output directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
 	plugins: [react(), svgr()],
 	build: {
-		outDir: '../public',
+		outDir: 'build',
 		emptyOutDir: true,
 	},
 })
